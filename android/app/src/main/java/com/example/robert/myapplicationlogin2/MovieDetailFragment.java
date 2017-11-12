4962a756167c06af54677b8cee7bc90b69b43810
robyerts <cazoni_robert@yahoo.com>
2017-11-13 00:28:11 +0200

    added android project

---
+++ android/app/src/main/java/com/example/robert/myapplicationlogin2/MovieDetailFragment.java
@@ -0,0 +1,114 @@
+package com.example.robert.myapplicationlogin2;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.robert.myapplicationlogin2.dummy.DummyContent;
+
+/**
+ * A fragment representing a single Movie detail screen.
+ * This fragment is either contained in a {@link MovieListActivity}
+ * in two-pane mode (on tablets) or a {@link MovieDetailActivity}
+ * on handsets.
+ */
+public class MovieDetailFragment extends Fragment {
+    /**
+     * The fragment argument representing the item ID that this fragment
+     * represents.
+     */
+    public static final String ARG_ITEM_ID = "item_id";
+
+    /**
+     * The dummy content this fragment is presenting.
+     */
+    private DummyContent.DummyItem mItem;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public MovieDetailFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments().containsKey(ARG_ITEM_ID)) {
+            // Load the dummy content specified by the fragment
+            // arguments. In a real-world scenario, use a Loader
+            // to load content from a content provider.
+            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+
+            Activity activity = this.getActivity();
+            CollapsingToolbarLayout appBarLayout = (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
+            if (appBarLayout != null) {
+                appBarLayout.setTitle(mItem.content);
+            }
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.movie_detail, container, false);
+
+        // Show the dummy content as text in a TextView.
+        if (mItem != null) {
+            EditText et =  rootView.findViewById(R.id.movie_detail1);
+            et.setText(mItem.details1);
+
+            et.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+                    mItem.details1 = s.toString();
+                }
+            });
+
+            EditText et2 =  rootView.findViewById(R.id.movie_detail2);
+            et2.setText(mItem.details2);
+
+            et2.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+                    mItem.details2 = s.toString();
+                }
+            });
+        }
+
+        return rootView;
+    }
+
+
+}
